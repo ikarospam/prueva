50e369134d5d0394160c90f13590ee0c5263023f
ikarospam <ikarospam@gmail.com>
2020-09-10 14:01:07 -0500

    nuevo

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet"  href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="">
+</head>
+<body>
+<h1>hola</h1>
+</body>
+<script src="node_modules/jquery/dist/jquery.js"></script>
+<script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+</html>
